007e12a17774bffaa77162ef6d1ab0ae68b130a7
el-ethan <elethan@gmail.com>
2016-03-01 22:04:08 -0600

    Add some failing tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,9 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-class TestNewVistor(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,7 +15,25 @@ class TestNewVistor(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a todo-item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the fucking tests already!')
 
 if __name__ == '__main__':
     unittest.main()
