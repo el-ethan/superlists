d59754b062c43d44a2e4079fa66ebc49b364ad0e
el-ethan <elethan@gmail.com>
2016-03-26 21:00:50 -0500

    Update tests and views

--- lists/tests.py
+++ lists/tests.py
@@ -15,12 +15,12 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         request.method = 'POST'
-        request.POST['item_text'] = 'a new list item'
+        request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
         expected_html = render_to_string(
             'home.html',
-            {'new_item_text': 'a new list item'},
+            {'new_item_text': 'A new list item'},
             request=request,
         )
 
@@ -35,8 +35,23 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
         self.assertIn('A new list item', response.content.decode())
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+
+        def test_home_page_only_saves_items_when_necessary(self):
+            request = HttpRequest()
+            home_page(request)
+            self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
